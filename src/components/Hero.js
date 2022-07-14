bc77d2360daac65313f2879f781604fbde932d37
Jake Walsh <jkwalsh127@gmail.com>
2022-07-13 21:25:00 -0700

    order refactored hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,16 +14,6 @@ function Hero({setPoster, poster}) {
         </video>  
         <img className={`${poster ? "image-display" : "image-hide"}`} src={photo} alt="hero" />
         <div className={`${poster ? "welcome-wrapper-poster" : "welcome-wrapper"}`}>      
-            <p className="welcome-header">
-                <span className="highlight-text">BTC</span> bot<br/>
-            </p>
-            <p className="welcome-subheader">
-                "The Workhorse"
-            </p>
-            <p className="welcome-description">
-                This bot is always in a position on Bitcoin.<br />
-                Long or short, it never misses a big move.
-            </p>
         </div>
     </div>
   );
