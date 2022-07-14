83c26dba1e3ab36d2549b345de6aef5d88572d3c
Jake Walsh <jkwalsh127@gmail.com>
2022-07-13 22:33:57 -0700

    provide external links to resume, portfolio, and social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,33 +3,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import resume from "./files/Resume (crypto).pdf";
 
 
-function Footer() {
+export default function Footer() {
   return (
     <footer className="footer-container">
         <div className='resume-wrapper'>
-            <a className='resume-link' href='./assets/files/Resume (crypto).pdf' download='jake-walsh-resume' target='_blank'>
+            <a className='resume-link' href={resume} download='jake-walsh-resume' target='_blank' rel='noreferrer'>
                 My Resume
             </a>
-            <a className='resume-link' href='#' target='_blank'>
+            <a className='resume-link' href='https://jkwalsh127.github.io/my-portfolio-page/' target='_blank' rel='noreferrer'>
                 My Porftolio
             </a>
         </div>
         <div class="socials-wrapper">
-            <a href="#" className='social-link'>
+            <a className='social-link' href="https://github.com/jkwalsh127" target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon 
                     icon={faGithub} 
                     className='social-icon github'
                 />
             </a>
-            <a href="#" className='social-link'>
+            <a className='social-link' href="https://www.linkedin.com/in/jake--walsh/" target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon 
                     icon={faLinkedin} 
                     className='social-icon linkedin'
                 />
             </a>
-            <a href="#" className='social-link'>
+            <a className='social-link' href='mailto:jkwalsh127@gmail.com' target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon 
                     icon={faEnvelope} 
                     className='social-icon email'
@@ -40,5 +41,3 @@ function Footer() {
     </footer>
   );
 };
-<i class=""></i>
-export default Footer;
